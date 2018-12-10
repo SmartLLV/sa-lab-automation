4a89b8f7fafeead40548143290c2f70978a828af
zhongqin0820 <zhongqin0820@163.com>
2018-12-10 11:45:35 +0800

    add api links

--- config/index.js
+++ config/index.js
@@ -30,7 +30,7 @@ module.exports = {
         proxyTable: {
             '/api':{
                 // target:'http://jsonplaceholder.typicode.com',
-                target: 'http://127.0.0.1:3000/api/',
+                target: 'https://sa-labauto.herokuapp.com/api/',
                 changeOrigin:true,
                 pathRewrite:{
                     '^/api':''
