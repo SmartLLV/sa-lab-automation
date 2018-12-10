e41be99208187517423df93e89cc13d15fbe5cc2
zhongqin0820 <zhongqin0820@163.com>
2018-12-10 10:52:53 +0800

    add backend

---
+++ service/db/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mysql: {
+        host: 'sql12.freemysqlhosting.net',
+        user: 'sql12269246',
+        password: 'jq1EleBJ8b',
+        port: '3306',
+        database: 'sql12269246'
+    }
+}
