06c7e351ccb0901881c063a1257e6f07434a18dc
zhongqin0820 <zhongqin0820@163.com>
2018-12-10 10:43:38 +0800

    hello world

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,30 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="login-wrap">
+    <div class="ms-title">{{ msg }}</div>
+    <div class="ms-login">
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm"
+      label-width="0px">
+        <div v-if="errorInfo">
+          <span>{{ errInfo }}</span>
+        </div>
+        <el-form-item prop="name">
+          <el-input v-model="ruleForm.name" placeholder="账号" ></el-input>
+        </el-form-item>
+        <el-form-item prop="passwd">
+          <el-input type="passwd" placeholder="密码" v-model="ruleForm.passwd"
+          @keyup.enter.native="submitForm('ruleForm')"></el-input>
+        </el-form-item>
+        <el-form-item  prop="validate">
+          <el-input v-model="ruleForm.validate" class="validate-code" placeholder="验证码" ></el-input>
+          <div class="code" @click="refreshCode">
+            <s-identify :identifyCode="identifyCode"></s-identify>
+          </div>
+        </el-form-item>
+        <div class="login-btn">
+          <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+        </div>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -88,26 +33,104 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      identifyCodes: '1234567890',
+      identifyCode: '',
+      msg: '登录',
+      errInfo: false,
+      ruleForm: {
+        name: '',
+        passwd: '',
+        validate: '',
+      },
+      rules: {
+        name: [
+          {
+            required: true,
+            message: 'please enter the username!',
+            trigger: 'blur',
+          },
+        ],
+        passwd: [
+          {
+            required: true,
+            message: 'please enter the passwd!',
+            trigger: 'blur',
+          },
+        ],
+        validate: [
+          {
+            required: true,
+            message: 'please enter the identify code!',
+            trigger: 'blur',
+          },
+        ],
+      },
     };
+  },
+  mounted() {
+    this.identifyCode = '';
+    this.makeCode(this.identifyCodes, 4);
+  },
+  methods: {
+    randomNum(min, max) {
+      return Math.floor((Math.random() * (max - min)) + min);
+    },
+    makeCode(o, l) {
+      for (let i = 0; i < l; i += 1) {
+        this.identifyCode += this.identifyCodes[this.randomNum(0, this.identifyCodes.length)];
+      }
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.login-wrap{
+  position: relative;
+  width:100%;
+  height:100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.ms-title{
+  position: absolute;
+  top:50%;
+  width:100%;
+  margin-top: -230px;
+  text-align: center;
+  font-size:30px;
+  color: #fff;
+
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.ms-login{
+  position: absolute;
+  left:50%;
+  top:50%;
+  width:300px;
+  height:240px;
+  margin:-150px 0 0 -190px;
+  padding:40px;
+  border-radius: 5px;
+  background: #fff;
 }
-a {
-  color: #42b983;
+.ms-login span {
+  color: red;
+}
+.login-btn{
+  text-align: center;
+}
+.login-btn button{
+  width:100%;
+  height:36px;
+}
+.code {
+  width: 112px;
+  height: 35px;
+  border: 1px solid #ccc;
+  float: right;
+  border-radius: 2px;
+}
+.validate-code {
+  width: 136px;
+  float: left;
 }
 </style>
