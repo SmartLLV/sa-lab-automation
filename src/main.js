f2bfdf0306f5b75066bb0a36a90b3760fc021b76
zhongqin0820 <zhongqin0820@163.com>
2018-12-10 10:52:38 +0800

    modify some changes

--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import ElementUI from 'element-ui';
 import App from './App';
 import router from './router';
+import axios from 'axios';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';    // 默认主题
+// import '../static/css/theme-green/index.css';       // 浅绿色主题
+import SIdentify from './components/page/Identify';    //自定义组件
+import "babel-polyfill";
 
-Vue.config.productionTip = false;
+Vue.component("SIdentify",SIdentify);
 Vue.use(ElementUI);
-/* eslint-disable no-new */
+
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+Vue.prototype.$http = window.axios
+
+// Vue.use(axios);
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>',
-});
+    router,
+    render: h => h(App)
+}).$mount('#app');
